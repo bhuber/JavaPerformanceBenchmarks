642617c3bddf2ff400f4b3da932274eaa6f96a09
Bennet Huber <bennet.huber@gmail.com>
2023-10-02 11:44:59 -0700

    Add hello test

--- java-perf-tests/build.gradle.kts
+++ java-perf-tests/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    kotlin("jvm") version "1.9.10"
 }
 
 group = "org.bhuber"
@@ -12,8 +13,13 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(19)
 }
---
+++ java-perf-tests/src/test/java/org/bhuber/java/collections/linkedlist/FooKtTest.kt
@@ -0,0 +1,13 @@
+package org.bhuber.java.collections.linkedlist
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class FooKtTest {
+
+    @Test
+    fun helloWorld() {
+        assertEquals("Hello, Foo", helloWorld("Foo"))
+    }
+}
